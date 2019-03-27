e1a0f706c563cb157f685f7c7c32d957c232d862
Tarnatos <tarnatos@posteo.de>
2019-03-27 08:24:48 +0100

    Update repos.pp

--- manifests/resources/repos.pp
+++ manifests/resources/repos.pp
@@ -4,28 +4,5 @@ class ffnord::resources::repos (
   package {
     'apt-transport-https':
       ensure => installed;
-  } ->
-  apt::source { 'repo.universe-factory':
-    location   => 'https://repo.universe-factory.net/debian/',
-    release    => 'sid',
-    repos      => 'main',
-    key        => '16EF3F64CB201D9C',
-    key_server => 'pgpkeys.mit.edu';
-  }
-
-  apt::source { 'debian.draic.info':
-    location    => 'http://debian.draic.info/',
-    release     => 'wheezy',
-    repos       => 'main',
-    include_src => false,
-    key_server  => 'pgpkeys.mit.edu';
-  }
-
-  apt::source { 'debian-backports':
-    location          => $debian_mirror,
-    required_packages => 'debian-keyring debian-archive-keyring',
-    release           => "${::lsbdistcodename}-backports",
-    repos             => 'main contrib',
-    include_src       => false,
-  }
+  } 
 }
